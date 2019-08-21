3f58e0ded0f13611a9feb2d2e1b4abfef420395e
Tu Nombre <you@example.com>
2019-08-20 19:53:41 -0500

    first commit

---
+++ Assets/Scripts/Esfera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Esfera : MonoBehaviour, IPoolObj
+{
+    public float lForce = 3f;
+
+    public void OnObjectSpawn()
+    {
+        float xForce = Random.Range(-lForce, lForce);
+        float zForce = Random.Range(-lForce, lForce);
+
+        Vector3 force = new Vector3(xForce, 40f, zForce);
+
+        GetComponent<Rigidbody>().velocity = force;
+    }
+}
---
+++ Assets/Scripts/EsferaSpawander.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EsferaSpawander : MonoBehaviour
+{
+    public string etiqueta;
+    ObjectPool objectPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPool = ObjectPool.Instance;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 randonPos = new Vector3(transform.position.x, Random.Range(0.5f, 2), transform.position.z);
+        objectPool.SpawnFromPool(etiqueta, randonPos, Quaternion.identity);
+    }
+}
